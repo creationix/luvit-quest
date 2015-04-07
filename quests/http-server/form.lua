112cbe730752d0a8414f21782ba3cd1b7299694e
Ryan Phillips <ryan.phillips@rackspace.com>
2015-04-07 09:53:41 -0700

    add tcp-server test

--- quests/http-server/form.lua
+++ quests/http-server/form.lua
@@ -2,7 +2,7 @@
 return function (req, res)
   local ip = req.socket:getpeername().ip
   res.body = res.body:gsub("<!%-%-fields%-%->",
-    '<p><form method="POST" action="/welcome/test">' ..
+    '<p><form method="POST" action="/tcp-server/test">' ..
       '<label for="host">Host:<input name="host" type="text" value="' .. ip .. '"></label>' ..
       '<label for="port">Port:<input name="port" type="text" value="8080"></label>' ..
       '<input type="submit" value="Test My Server">' ..
--- quests/tcp-server/route.lua
+++ quests/tcp-server/route.lua
@@ -1,2 +1,17 @@
+local queryParse = require('querystring').parse
+local tcp = require('coro-tcp')
 return function (app)
+  app.route({
+    method = "POST",
+    path = "/tcp-server/test"
+  }, function (req, res)
+    if not req.validate("welcome") then return end
+    local config = queryParse(req.body)
+    local read, write = tcp.connect(config.host, config.port)
+    if not read then error(write) end
+    local signal = 'guybrush threepwood'
+    write(signal)
+    local data = read()
+    if data == signal then return res.teleport("tcp-client") end
+  end)
 end
